f3b4e650f2e32ddcd8ae4d0c8e0a51e9b232f6f6
Idris_ch <cherietidrisse1@gmail.com>
2025-08-18 11:12:15 +0100

    first commit

---
+++ src/main/java/com/crypto/cryotoMtlsServer/CryotoMtlsServerApplication.java
@@ -0,0 +1,14 @@
+package com.crypto.cryotoMtlsServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryotoMtlsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryotoMtlsServerApplication.class, args);
+	}
+
+	          
+}
